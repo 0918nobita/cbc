5131866465d896eb5269acf622d0698e3341edc7
0918nobita <nobita.0918@gmail.com>
2021-10-13 23:03:01 +0900

    Lexer をリファクタリングした

--- src/main/kotlin/vision/kodai/cbc/Lexer.kt
+++ src/main/kotlin/vision/kodai/cbc/Lexer.kt
@@ -8,45 +8,46 @@ enum class UnaryOp { PLUS, MINUS }
 
 sealed interface LexerState {
     object Initial : LexerState
-
-    data class ShouldReceiveDigit(val unaryOp: UnaryOp) : LexerState
-
-    data class CanReceiveAdditionalDigit(
-        val unaryOp: UnaryOp,
-        val digits: String
-    ) : LexerState
 }
+
+data class ShouldReceiveDigit(val unaryOp: UnaryOp) : LexerState
+
+data class CanReceiveAdditionalDigit(
+    val unaryOp: UnaryOp,
+    val digits: String
+) : LexerState
 
 class LexerException(msg: String) : Exception(msg)
 
 class Lexer(reader: BufferedReader) {
     val tokens = flow {
-        val chars = reader.lineSequence().flatMap { it.toList() }
-        var state: LexerState = LexerState.Initial
-        var numChars = -1
-
-        for (c in chars) {
-            state = when (state) {
-                is LexerState.Initial -> when {
-                    c == '+' -> LexerState.ShouldReceiveDigit(UnaryOp.PLUS)
-                    c == '-' -> LexerState.ShouldReceiveDigit(UnaryOp.MINUS)
-                    c.isDigit() -> LexerState.CanReceiveAdditionalDigit(UnaryOp.PLUS, c.toString())
-                    else -> throw LexerException("Expected +, - or digit")
+        val (state, numChars) =
+            reader
+                .lineSequence()
+                .flatMap { it.toList() }
+                .runningFold<Char, Pair<LexerState, Int>>(Pair(LexerState.Initial, -1)) { (prevState, numChars), c ->
+                    val state = when (prevState) {
+                        is LexerState.Initial -> when {
+                            c == '+' -> ShouldReceiveDigit(UnaryOp.PLUS)
+                            c == '-' -> ShouldReceiveDigit(UnaryOp.MINUS)
+                            c.isDigit() -> CanReceiveAdditionalDigit(UnaryOp.PLUS, c.toString())
+                            else -> throw LexerException("Expected +, - or digit")
+                        }
+                        is ShouldReceiveDigit -> when {
+                            c.isDigit() -> CanReceiveAdditionalDigit(prevState.unaryOp, c.toString())
+                            else -> throw LexerException("Expected digit")
+                        }
+                        is CanReceiveAdditionalDigit -> when {
+                            c.isDigit() -> CanReceiveAdditionalDigit(prevState.unaryOp, prevState.digits + c)
+                            else -> throw LexerException("Expected digit")
+                        }
+                    }
+                    Pair(state, numChars + 1)
                 }
-                is LexerState.ShouldReceiveDigit -> when {
-                    c.isDigit() -> LexerState.CanReceiveAdditionalDigit(state.unaryOp, c.toString())
-                    else -> throw LexerException("Expected digit")
-                }
-                is LexerState.CanReceiveAdditionalDigit -> when {
-                    c.isDigit() -> LexerState.CanReceiveAdditionalDigit(state.unaryOp, state.digits + c)
-                    else -> throw LexerException("Expected digit")
-                }
-            }
-            numChars++
-        }
+                .last()
 
         when (state) {
-            is LexerState.CanReceiveAdditionalDigit -> {
+            is CanReceiveAdditionalDigit -> {
                 val begin = Point(0, 0)
                 val end = Point(0, numChars)
                 val intVal = state.digits.toInt() * if (state.unaryOp == UnaryOp.MINUS) {
--- src/main/kotlin/vision/kodai/cbc/Main.kt
+++ src/main/kotlin/vision/kodai/cbc/Main.kt
@@ -1,16 +1,21 @@
 package vision.kodai.cbc
 
+import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.runBlocking
 import java.io.BufferedReader
 import java.io.StringReader
+import kotlin.system.exitProcess
 
 fun main(args: Array<String>) {
     Compiler.main(args)
 
     val lexer = Lexer(BufferedReader(StringReader("-42")))
     runBlocking {
-        lexer.tokens.collect {
+        lexer.tokens.catch {
+            System.err.println(it.message)
+            exitProcess(1)
+        }.collect {
             println(it)
         }
     }
