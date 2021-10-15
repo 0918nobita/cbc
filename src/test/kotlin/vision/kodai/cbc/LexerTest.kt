cac582b93679571a13c4702d9c56e040101e0f5a
0918nobita <nobita.0918@gmail.com>
2021-10-15 23:22:20 +0900

    :white_check_mark: Kotest を導入した

--- src/test/kotlin/vision/kodai/cbc/LexerTest.kt
+++ src/test/kotlin/vision/kodai/cbc/LexerTest.kt
@@ -1,16 +1,13 @@
 package vision.kodai.cbc
 
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.shouldBe
 import kotlinx.coroutines.flow.asFlow
 import kotlinx.coroutines.flow.toList
-import kotlinx.coroutines.test.runBlockingTest
-import org.junit.jupiter.api.Test
 import vision.kodai.cbc.token.IntToken
-import kotlin.test.assertEquals
 
-object LexerTest {
-    @Test
-    @kotlinx.coroutines.ExperimentalCoroutinesApi
-    fun tokenization() {
+class LexerTest : StringSpec({
+    "tokenization" {
         val src = "3 7\n    21\n"
         val tokenFlow = src.toList().asFlow().lex()
         val expected =
@@ -19,9 +16,7 @@ object LexerTest {
                 IntToken(Point(0, 2), Point(0, 2), 7),
                 IntToken(Point(1, 4), Point(1, 5), 21)
             )
-        runBlockingTest {
-            val tokens = tokenFlow.toList()
-            assertEquals(expected, tokens)
-        }
+        val tokens = tokenFlow.toList()
+        expected shouldBe tokens
     }
-}
+})
