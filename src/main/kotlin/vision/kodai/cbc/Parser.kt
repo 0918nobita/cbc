7d9aec80b5d6b5674362efe67623225538bcf821
0918nobita <nobita.0918@gmail.com>
2021-10-18 16:36:27 +0900

    :construction: mapFirst 拡張関数を定義してリファクタリングした

--- src/main/kotlin/vision/kodai/cbc/Parser.kt
+++ src/main/kotlin/vision/kodai/cbc/Parser.kt
@@ -25,19 +25,36 @@ suspend fun Flow<Token>.parse(): Entity.Expr<Int> {
 
 suspend fun <T> Flow<T>.firstAndTail(): Pair<T, Flow<T>> = Pair(first(), drop(1))
 
+suspend fun <T> Flow<T>.isEmpty() = count() == 0
+
+fun <A1, A2, B> Pair<A1, Flow<B>>.mapFirst(f: (A1) -> A2): Pair<A2, Flow<B>> =
+    Pair(f(first), second)
+
+/*
+suspend fun <A1, A2, B> Pair<A1, Flow<B>>.chain(f: suspend (A1) -> Pair<A2, Flow<B>>): Pair<A2, Flow<B>> =
+    if (second.isEmpty()) { f(first) }
+*/
+
 /** 式のパーサ */
 @Throws(ParseException::class)
 suspend fun Flow<Token>.expr(): Pair<Entity.Expr<Int>, Flow<Token>> {
     val (lhs, rem1) = intLiteral()
-    if (rem1.count() == 0) return Pair(lhs, rem1)
+    if (rem1.isEmpty()) return Pair(lhs, rem1)
 
     val (op, rem2) = rem1.firstAndTail()
     if (op !is Token.Plus)
         throw ParseException(ParseExnReason.UnexpectedToken("+", op))
 
-    val (rhs, rem3) = rem2.intLiteral()
-
-    return Pair(Entity.Expr.Add(lhs, rhs, lhs.begin, rhs.end), rem3)
+    return rem2.intLiteral().mapFirst { rhs -> Entity.Expr.Add(lhs, rhs, lhs.begin, rhs.end) }
+    /*
+    return intLiteral()
+        .chain { lhs ->
+            firstAndTail().chain { op ->
+                if (op !is Token.Plus) throw ParseException(ParseExnReason.UnexpectedToken("+", op))
+                intLiteral().mapFirst { rhs -> Entity.Expr.Add(lhs, rhs, lhs.begin, rhs.end) }
+            }
+        }
+     */
 }
 
 /** 整数リテラルのパーサ */
