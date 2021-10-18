9910c5d7a22c62a2c558765104bcd3200d4c4fb2
0918nobita <nobita.0918@gmail.com>
2021-10-18 16:55:25 +0900

    :construction: run 拡張関数を用いてリファクタリングした

--- src/main/kotlin/vision/kodai/cbc/Parser.kt
+++ src/main/kotlin/vision/kodai/cbc/Parser.kt
@@ -25,49 +25,30 @@ suspend fun Flow<Token>.parse(): Entity.Expr<Int> {
 
 suspend fun <T> Flow<T>.firstAndTail(): Pair<T, Flow<T>> = Pair(first(), drop(1))
 
-suspend fun <T> Flow<T>.isEmpty() = count() == 0
-
 fun <A1, A2, B> Pair<A1, Flow<B>>.mapFirst(f: (A1) -> A2): Pair<A2, Flow<B>> =
     Pair(f(first), second)
 
-/*
-suspend fun <A1, A2, B> Pair<A1, Flow<B>>.chain(f: suspend (A1) -> Pair<A2, Flow<B>>): Pair<A2, Flow<B>> =
-    if (second.isEmpty()) { f(first) }
-*/
-
 /** 式のパーサ */
 @Throws(ParseException::class)
-suspend fun Flow<Token>.expr(): Pair<Entity.Expr<Int>, Flow<Token>> {
-    val (lhs, rem1) = intLiteral()
-    if (rem1.isEmpty()) return Pair(lhs, rem1)
-
-    val (op, rem2) = rem1.firstAndTail()
-    if (op !is Token.Plus)
-        throw ParseException(ParseExnReason.UnexpectedToken("+", op))
-
-    return rem2.intLiteral().mapFirst { rhs -> Entity.Expr.Add(lhs, rhs, lhs.begin, rhs.end) }
-    /*
-    return intLiteral()
-        .chain { lhs ->
-            firstAndTail().chain { op ->
-                if (op !is Token.Plus) throw ParseException(ParseExnReason.UnexpectedToken("+", op))
-                intLiteral().mapFirst { rhs -> Entity.Expr.Add(lhs, rhs, lhs.begin, rhs.end) }
-            }
+suspend fun Flow<Token>.expr() =
+    intLiteral().run {
+        val lhs = first
+        second.firstAndTail().run {
+            val op = first
+            if (op !is Token.Plus) throw ParseException(ParseExnReason.UnexpectedToken("+", op))
+            second.intLiteral().mapFirst { rhs -> Entity.Expr.Add(lhs, rhs, lhs.begin, rhs.end) }
         }
-     */
-}
+    }
 
 /** 整数リテラルのパーサ */
 @Throws(ParseException::class)
-suspend fun Flow<Token>.intLiteral(): Pair<Entity.Expr.IntLiteral, Flow<Token>> {
-    val (token, rem) = try {
+suspend fun Flow<Token>.intLiteral() =
+    try {
         firstAndTail()
     } catch (e: NoSuchElementException) {
         throw ParseException(ParseExnReason.UnexpectedEOF("int"))
+    }.mapFirst { token ->
+        if (token !is Token.IntToken)
+            throw ParseException(ParseExnReason.UnexpectedToken("int", token))
+        Entity.Expr.IntLiteral(token.raw, token.begin, token.end)
     }
-
-    if (token !is Token.IntToken)
-        throw ParseException(ParseExnReason.UnexpectedToken("int", token))
-
-    return Pair(Entity.Expr.IntLiteral(token.raw, token.begin, token.end), rem)
-}
